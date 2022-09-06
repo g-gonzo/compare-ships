6ad6a1000cc605bfb30ddc188a93777787ddc2d5
Gilbert Gonzales <gigonz@extendhealth.com>
2022-09-06 16:45:13 -0600

    Initial upload of files

---
+++ script.js
@@ -0,0 +1,26 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
+function SearchForShips() {
+    console.log("searching for ships");
+};
+
+function SelectShipByGenre() {
+
+};
+
+function SelectShipByClass() {
+
+};
+
+function SelectShipBySize() {
+
+};
+
+function SelectShipBySpeed() {
+
+};
+
+document.querySelector('#submit').addEventListener('click', SearchForShips);
