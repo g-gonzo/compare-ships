03d187e60174ad6756771a605fed8ea1b96971ca
Gilbert Gonzales <gigonz@extendhealth.com>
2022-09-08 08:28:23 -0600

    updat to json for button

--- script.js
+++ script.js
@@ -9,30 +9,20 @@ let v1Submit = document.getElementById("submit1");
 // Button Listener
 v1Submit.addEventListener('click', function () {
     SearchForShips();
+    //SelectShipByGenre();
+
 });
 
-
-
 function SearchForShips() {
-    let results = [];
+    //let results = [];
     fetch('./db.json')
         .then(results => results.json())
         .then(console.log);
 };
 
 function SelectShipByGenre() {
-
+    let shiplist = results;
+    fetch('./db.json')
+        .then(results => results.json())
+        .then(console.log(shiplist));
 };
-
-function SelectShipByClass() {
-
-};
-
-function SelectShipBySize() {
-
-};
-
-function SelectShipBySpeed() {
-
-};
-
