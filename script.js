0d5bbca4ce447ccbd2e2d1dffed522a437a70eaa
Gilbert Gonzales <gigonz@extendhealth.com>
2022-09-15 21:30:26 -0600

    added a bunch of json stuff

--- script.js
+++ script.js
@@ -10,24 +10,26 @@ let btn2 = document.getElementById("btn-2");
 let btn3 = document.getElementById("btn-3");
 let btn4 = document.getElementById("btn-4");
 
-// Attempt at creating a list of ships with JSON Obj
-//const obj = JSON.parse(Ships);
-
 // Button Listeners
 v1Submit.addEventListener('click', function () {
     SearchForShips();
 });
 
+//Buttons 1 - 4 really do not appear to be used, kinda using them to test stuff in console. 
 btn1.addEventListener('click', function () {
     console.log("Button 1 has been clicked");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let out = "";
+            let ship1 = Ships[15];
+            for (let ship of Ships) {
 
-    let data = fetch('./db.json')
-        .then(results => results.json())
-        .then(console.log);
-
-    let ship1 = data.ship1[5];
-    console.log(ship1);
-
+            }
+            console.log(ship1);
+        })
 });
 
 btn2.addEventListener('click', function () {
@@ -43,33 +45,175 @@ btn4.addEventListener('click', function () {
 });
 
 function SearchForShips() {
-    fetch('./db.json')
-        .then(results => results.json())
-        .then(console.log);
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                out +=
+                    `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+            }
+            placeholder.innerHTML = out;
+        })
 };
 
 function SelectStarTrek() {
     console.log("Selecting StarTrek ships !!!");
-}
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Genre === "StarFleet") {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
+};
 
 function SelectStarWars() {
     console.log("Selecting StarWars ships !!!");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Genre === "StarWars") {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
 }
 
-function SelectCorrvette() {
+function SelectCorvette() {
     console.log("Selecting Class Corrvette !!!");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Model == "Corvette") {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
 }
 
 function SelectDestroyer() {
     console.log("Selecting Class Destroyer !!!");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Model.includes("Destroyer")) {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
 }
 
 function SelectFrigate() {
     console.log("Selecting Class Frigate !!!");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Model.includes("Frigate")) {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
 }
 
 function SelectCruiser() {
     console.log("Selecting Class Cruiser !!!");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Model.includes("Cruiser")) {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
 }
 
 function SelectBattleCraft() {
@@ -82,6 +226,27 @@ function SelectSupportCraft() {
 
 function SelectSmallShips() {
     console.log("Selecting Small Ships !!!");
+    fetch("Ships.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (Ships) {
+            let placeholder = document.querySelector("#data-output");
+            let out = "";
+            for (let ship of Ships) {
+                if (ship.Displacement.sort()) {
+                    out +=
+                        `<tr>
+                        <td>${JSON.stringify(ship.Id)}</td>
+                        <td>${JSON.stringify(ship.Name)}</td>
+                        <td>${JSON.stringify(ship.Model)}</td>
+                        <td>${JSON.stringify(ship.Genre)}</td>
+                    </tr>
+                    `;
+                }
+            }
+            placeholder.innerHTML = out;
+        })
 }
 
 function SelectLargeShips() {
